c5497cfa0d27ad97d1904a2f4aea70ee67fc8b46
sutobuto <estabillog8@gmail.com>
2026-02-24 01:10:09 +0800

    feat: added alternate art categorization for getPrice endpoint

--- src/services/yuyutei.services.js
+++ src/services/yuyutei.services.js
@@ -4,12 +4,13 @@ export const getPrice = async (name) => {
     // initialize browser
     const url = `https://yuyu-tei.jp/sell/ygo/s/search?search_word=${name}`;
     console.log(url);
-    const $ = await cheerio.fromURL(url);
+    const $ = await cheerio.fromURL(url, {
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
+        }
+    });
     
-    const $rarities = $('span.py-2');
     const $rarityCardGroups = $('div#card-list3')
-    
-    const rarities = [];
     const data = [];
 
     // TODO: seperate alternate art pricing
@@ -17,10 +18,20 @@ export const getPrice = async (name) => {
         const $cardGroup = $(rarity).find('div.card-product');
         $cardGroup.each((i, card) => {
             console.log($(card).find('span').text());
+
+            var isAltArt = false;
+            const yenPrice = $(card).find('strong').text().replaceAll(',', '').replaceAll(/\n/g, '').replace('円', '').replaceAll(' ', '');
+            const phpPrice = yenPrice * 0.37;
+
+            const $cardname = $(card).find('h4').text();
+            if($cardname.includes('(イラスト違い版)')) isAltArt = true;
+
             data.push({ 
                 rarity: $(rarity).find('span.py-2').text(), 
                 setID: $(card).find('span').text(), 
-                yenPrice: $(card).find('strong').text().replace(/\n/g, '').replace('円', '').replace(' ', '')
+                yenPrice: parseInt(yenPrice, 10),
+                phpPrice: phpPrice,
+                isAltArt: isAltArt
             });
         })
     })
